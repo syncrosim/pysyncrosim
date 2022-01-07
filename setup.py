2fcabd1d24ef200e9d8705bc7a1523107713c4fc
Katie Birchard <33553358+katieb1@users.noreply.github.com>
2022-01-07 13:04:50 -0800

    Incrementing version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name="pysyncrosim",
-      version="1.0.1",
+      version="1.0.2",
       description="Python interface to SyncroSim",
       author="ApexRMS",
       author_email="info@apexrms.com",
