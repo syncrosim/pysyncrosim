91ef364a1acc17a73757d7099c745e30d88b8770
katieb1 <birchardkatie@gmail.com>
2024-01-03 09:52:09 -0800

    Update copyright to 2024

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'pysyncrosim'
-copyright = '2023, ApexRMS'
+copyright = '2024, ApexRMS'
 author = 'ApexRMS'
 
 # The full version, including alpha/beta/rc tags
