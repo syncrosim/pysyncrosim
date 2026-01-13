a428354125394aea6849e79d8eb7e393bc3166ac
afilazzola <alex.filazzola@apexrms.com>
2026-01-13 12:32:10 -0500

    removed panda dependency when loading version

--- setup.py
+++ setup.py
@@ -3,21 +3,27 @@
 
 from setuptools import setup, find_packages
 from pathlib import Path
-from pysyncrosim._version import __version__
 
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
-setup(name="pysyncrosim",
-      version=__version__,
-      description="Python interface to SyncroSim",
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      author="ApexRMS",
-      author_email="info@apexrms.com",
-      url="https://syncrosim.com/",
-      packages=find_packages(exclude="tests"))
+# Read version without importing the package
+version = {}
+exec((this_directory / "pysyncrosim" / "_version.py").read_text(encoding="utf-8"), version)
+__version__ = version["__version__"]
 
-install_requires=['numpy', 'pandas', 'rasterio']
-
-extras_require=['numpy', 'pandas', 'rasterio']
+setup(
+    name="pysyncrosim",
+    version=__version__,
+    description="Python interface to SyncroSim",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="ApexRMS",
+    author_email="info@apexrms.com",
+    url="https://syncrosim.com/",
+    packages=find_packages(exclude=("tests",)),
+    install_requires=["numpy", "pandas", "rasterio"],
+    extras_require={
+        "dev": ["pytest", "build", "twine"],
+    },
+)
